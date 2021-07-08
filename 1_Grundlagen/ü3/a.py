a003f19796bf0f6013382f5162a55631e0082f33
Fabian <fab.beringer@gmail.com>
2021-07-08 20:49:36 +0200

    solve 1.3.

--- 1_Grundlagen/ü3/a.py
+++ 1_Grundlagen/ü3/a.py
@@ -1,7 +1,8 @@
-import numpy as np
 import cv2
 
 # Hiermit kann die Methode für die Berechnung ausgewählt werden
+import numpy as np
+
 METHOD = "MANUELL"  # OpenCV
 
 # Einlesen des Bildes
@@ -15,8 +16,28 @@ print("Originale Höhe:", h)
 scales = [4, 8, 13.5]
 images = []
 
+
 # todo Methode MANUELL implementieren und 'images' mit diskretisierten Bildern füllen
+def discretize(im, k):
+    h_old, w_old, _ = im.shape
+    h_bound, w_bound = h_old - 1, w_old - 1
+    for i in np.arange(0, h_old, k):
+        for j in np.arange(0, w_old, k):
+            i_to = min(round(i + k), h_bound)
+            j_to = min(round(j + k), w_bound)
+
+            i = min(round(i), h_bound)
+            j = min(round(j), w_bound)
+
+            im[i:i_to, j:j_to] = img[i, j]
+    return im
+
+
+cv2.imshow('flower', discretize(img, 41.5214242))
+cv2.waitKey()
+
 
 # todo Methode OpenCV implementieren und 'images' mit diskretisierten Bildern füllen
+
 
 # todo Bilder darstellen
--- 1_Grundlagen/ü3/b.py
+++ 1_Grundlagen/ü3/b.py
@@ -1,5 +1,5 @@
-import numpy as np
 import cv2
+import numpy as np
 
 # Hiermit kann die Methode für die Berechnung ausgewählt werden
 METHOD = "MANUELL"  # OpenCV
@@ -10,4 +10,19 @@ img = cv2.imread(filepath)
 
 h, w, c = img.shape
 
-# todo Schritte 1-3 implementieren!
+
+def quantize(im, low_new, high_new, low_old=0, high_old=255):
+    im = im.astype(float)
+    im = low_new + (high_new - low_new) * (im - low_old) / (high_old - low_old)
+    return im.astype(np.uint8)
+
+
+ranges = [(0, 127), (0, 3), (0, 255)]
+
+prev = (0, 255)
+for a, b in ranges:
+    img = quantize(img, a, b, *prev)
+    cv2.imshow('flower', img)
+    cv2.waitKey()
+
+    prev = a, b
